c3666d066a150ad718cba67d742544055067bb45
UPCHAUHAN <udaypratap1995@gmail.com>
2019-07-26 01:24:32 +0530

    firstcommit

---
+++ selenium_demo/src/demo/Demo.java
@@ -0,0 +1,21 @@
+package demo;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+
+public class Demo {
+
+	public static void main(String[] args) {
+
+		System.setProperty("webdriver.ie.driver","G:\\Selenium_Jar\\IEDriverServer.exe");
+		WebDriver driver = new InternetExplorerDriver();
+		driver.get("https://www.google.com/");
+		
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		
+
+	}
+
+}
